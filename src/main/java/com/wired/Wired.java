f5f66a20218ffd4e0e946842806db7cdda810e26
Gabriel Sá <gabriel10043@live.com>
2025-09-08 21:06:12 -0300

    add application main class

---
+++ src/main/java/com/wired/Wired.java
@@ -0,0 +1,10 @@
+package com.wired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wired {
+	public static void main(String[] args){
+		SpringApplication.run(Wired.class, args);
+	}
+}
